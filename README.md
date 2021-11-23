a538efe9a64ece85eead71ce50f44c2be04b9fa2
sean_koh <sean.koh.2015@smu.edu.sg>
2021-11-23 14:07:50 +0800

    Add summary function

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # quote_extract
 
-To run this: 
 
-`pip install -r requirements.txt`
-`streamlit run app/streamlit_app`
+To Setup Your Venv: 
+`python -m venv venv` </br>
+`source env/bin/activate` </br>
+`python -m pip install -r requirements.txt` </br>
+`python` </br>
+`import nltk` </br>
+`nltk.download('punkt')` </br>
+
+To Run This: 
+`python -m streamlit run app/streamlit_app.py` 
 
 Have fun!
--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import spacy
 import textacy.extract 
 from newspaper import Article
+import nltk
 import pandas as pd 
 
 nlp = spacy.load('en_core_web_lg')
@@ -11,11 +12,16 @@ st.title("News Article Quote Extraction Tool")
 url = st.text_input('Input your URL here:') 
 
 if url:
-    st.header("Extracted quotes from: "+url)
+    st.header("Keywords and Extracted Quotes from: "+url)
 
     article = Article(url)
     article.download()
     article.parse()
+
+    article.nlp()
+    st.code(article.keywords)
+    st.header('Summary: ')
+    st.write(article.summary)
 
     doc = nlp(article.text.replace("'"," "))
 
@@ -25,24 +31,25 @@ if url:
     dq2 = textacy.extract.direct_quotations(doc)
 
 
-    df = pd.DataFrame(dq2, columns=['speaker', 'verb', 'statement' ])
+    df = pd.DataFrame(dq2, columns=['speaker', 'verb', 'statement'])
+    df['source'] = url
 
     def convert_df(df):
-        return df.to_csv(index=False).encode('utf-8')
+        return df.to_csv().encode('utf-8')
 
-
-    # st.dataframe(df)
 
     csv = convert_df(df)
+    st.header("Direct Quotes:")
+
 
     for statement in dq:
         speaker, sig, stmt = statement
         st.markdown(f" > {speaker}: {stmt}")
 
     st.download_button(
-    "Press to Download",
+    "Click Here to Download Quotes",
     csv,
-    "quotes.csv",
+    article.title+".csv",
     "text/csv",
     key='download-csv'
     )
