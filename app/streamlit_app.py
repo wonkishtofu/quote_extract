1b1449203830830e0484034a566f4878c5fd054b
sean_koh <sean.koh.2015@smu.edu.sg>
2021-11-23 08:58:44 +0800

    Code Refactor

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,35 +1,55 @@
 import streamlit as st
 import spacy
 import textacy.extract 
-nlp = spacy.load('en_core_web_lg')
 from newspaper import Article
+import pandas as pd 
 
 nlp = spacy.load('en_core_web_lg')
-nlp.add_pipe("merge_entities")
-
 st.title("News Article Quote Extraction Tool")
 
-url = 'https://www.straitstimes.com/singapore/spore-to-relook-long-term-approach-to-land-use-planning-engagement-for-review-to-start'
 
 url = st.text_input('Input your URL here:') 
 
-
 if url:
     st.header("Extracted quotes from: "+url)
-
 
     article = Article(url)
     article.download()
     article.parse()
 
-    doc = nlp(article.text)
+    doc = nlp(article.text.replace("'"," "))
 
-
-    new_words = [token.text if token.text!="'" else "`" for token in doc]
-    
 
     dq = textacy.extract.direct_quotations(doc)
+
+    dq2 = textacy.extract.direct_quotations(doc)
+
+
+    df = pd.DataFrame(dq2, columns=['speaker', 'verb', 'statement' ])
+
+    def convert_df(df):
+        return df.to_csv(index=False).encode('utf-8')
+
+
+    # st.dataframe(df)
+
+    csv = convert_df(df)
 
     for statement in dq:
         speaker, sig, stmt = statement
         st.markdown(f" > {speaker}: {stmt}")
+
+    st.download_button(
+    "Press to Download",
+    csv,
+    "quotes.csv",
+    "text/csv",
+    key='download-csv'
+    )
+    
+    
+    
+
+
+
+    
